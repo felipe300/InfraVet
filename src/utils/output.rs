ed97eb5d3bc5f4530f0d3716f399d0863a004fe6
Felipe Gutiérrez <caracoloro@gmail.com>
2026-09-10 22:04:19 -0300

    test: add tests for output.rs

--- src/utils/output.rs
+++ src/utils/output.rs
@@ -1,6 +1,5 @@
-use std::path::Path;
-
 use colored::Colorize;
+use std::path::Path;
 
 use crate::analyzer::issue::{RuleId, Severity};
 
@@ -32,6 +31,40 @@ pub fn issue(severity: &Severity, rule: RuleId, line: usize, message: &str) {
 }
 
 pub fn highlight_path(path: &Path) -> String {
-    // path.bright_magenta().underline().to_string()
     path.display().to_string().cyan().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_highlight_path() {
+        let path = Path::new("/home/user/project/Dockerfile");
+        let result = highlight_path(path);
+
+        assert!(result.contains("/home/user/project/Dockerfile"));
+    }
+
+    #[test]
+    fn test_highlight_path_relative() {
+        let path = "app/Dockerfile.dev".to_string();
+        let relative_path = Path::new(&path);
+        assert!(highlight_path(relative_path).contains(&path));
+    }
+
+    #[test]
+    fn test_highlight_empty_path() {
+        let path = ".".to_string();
+        let empty_path = Path::new(&path);
+        assert!(highlight_path(empty_path).contains(&path));
+    }
+
+    #[test]
+    fn test_highlight_path_windows_and_special_chars() {
+        let path = "my folder/Docker file".to_string();
+        let path_with_spaces = Path::new(&path);
+        assert!(highlight_path(path_with_spaces).contains(&path));
+    }
 }
